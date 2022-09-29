317a0f0dacb05a609462d3c8310b1e6af1fe3b1f
JustinLien0805 <6998dragoan@gmail.com>
2022-09-29 11:54:22 +0800

    init

--- swift-guess-flag/ContentView.swift
+++ swift-guess-flag/ContentView.swift
@@ -8,15 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    //  randomizing the array order
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color.mint
+                .ignoresSafeArea()
+            VStack(spacing: 30){
+                VStack{
+                    Text("Tap the flag of: ")
+                        .font(.system(size: 30, weight: .bold))
+                    Text(countries[correctAnswer])
+                        .font(.system(size: 30, weight: .bold))
+                }
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }.alert(scoreTitle, isPresented: $showingScore) {
+                Button("Continue", action: askQuestion)
+            } message: {
+                Text("Your score is \(score)")
+            }
         }
-        .padding()
+
     }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score = score + 1
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
