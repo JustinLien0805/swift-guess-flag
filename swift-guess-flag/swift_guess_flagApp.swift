936c7c40ff2cc46d3d710ec09ae575e6eb803f87
JustinLien0805 <6998dragoan@gmail.com>
2022-09-29 09:24:27 +0800

    Initial Commit

---
+++ swift-guess-flag/swift_guess_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_guess_flagApp.swift
+//  swift-guess-flag
+//
+//  Created by 練家辰 on 2022/9/29.
+//
+
+import SwiftUI
+
+@main
+struct swift_guess_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
